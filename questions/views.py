4865eef9b0d93bd41d9b84c3cf66881a521e7bb0
Ninefiveblade <cherep1.92@yandex.ru>
2022-04-14 21:33:31 +0300

    mend

--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
-from django.views.generic import DetailView
 
 from .models import Answer, Result, Test, Question
 
@@ -36,23 +35,17 @@ def vote(request, pk):
     test = Test.objects.get(id=pk)
     print(request.POST)
     if request.POST.get('answer'):
-        selected_answers = Answer.objects.filter(id=request.POST['answer'])
-        storage_answers = {}
-        for answer in selected_answers:
-            answer.vote = 0
-            print(answer.vote)
-            storage_answers[answer.answer] = [answer.correct]
-            if storage_answers[answer.answer][0]:
-                answer.vote += 1
-                print(answer.vote)
-                answer.save()
-            else:
-                template = 'questions/tests.html'
-                context = {
-                    'error_message': f'Ваш ответ: {answer.answer} неверный',
-                    'correct_test': f'Правильные ответы:{test.questions.filter(answers__correct=True)}'
-                }
-                return render(request, template, context)
+        answer = Answer.objects.filter(id=request.POST['answer'])
+        print(answer)
+        if answer.correct:
+            answer.save()
+        else:
+            template = 'questions/tests.html'
+            context = {
+                'error_message': f'Ваш ответ: {answer.answer} неверный',
+                'correct_test': f'Правильные ответы:{test.questions.filter(answers__correct=True)}'
+            }
+            return render(request, template, context)
         return redirect('questions:result', pk)
     else:
         selected_answers = Answer.objects.filter(id=request.POST['answer'])
